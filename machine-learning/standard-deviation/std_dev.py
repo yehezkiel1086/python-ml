e0546c0c4a74cd0c5b9e33d92026b1315390bde2
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-04-30 07:16:44 +0700

    feat: update machine-learning/std_dev

--- machine-learning/standard-deviation/std_dev.py
+++ machine-learning/standard-deviation/std_dev.py
@@ -1,17 +1,23 @@
 '''
-standard deviation, variance
+Find the standard deviation and variance of the following datas:
+speed_1 = [86,87,88,86,87,85,86]
+speed_2 = [32,111,138,28,59,77,97] 
 '''
 #!/bin/python
 
 import numpy
 
-speeds_1 = [86,87,88,86,87,85,86]
-speeds_2 = [32,111,138,28,59,77,97]
+speed_1 = [86,87,88,86,87,85,86]
+speed_2 = [32,111,138,28,59,77,97]
 
-std_dev_1 = numpy.std(speeds_1)
-var_1 = numpy.var(speeds_1)
-std_dev_2 = numpy.std(speeds_2)
-var_2 = numpy.var(speeds_2)
+dev_1 = numpy.std(speed_1)
+dev_2 = numpy.std(speed_2)
 
-print(std_dev_1, std_dev_2)
-print(var_1, var_2)
+var_1 = numpy.var(speed_1)
+var_2 = numpy.var(speed_2)
+
+print(f"Standard deviation 1: {dev_1}")
+print(f"Standard deviation 2: {dev_2}")
+
+print(f"Variance 1: {var_1}")
+print(f"Variance 2: {var_2}")
