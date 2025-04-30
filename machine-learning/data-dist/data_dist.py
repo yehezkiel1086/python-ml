b8e9a3d25c0696263b59b547e42c68e198e3d53f
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-04-30 07:51:30 +0700

    feat: update machine-learning/data-dist

--- machine-learning/data-dist/data_dist.py
+++ machine-learning/data-dist/data_dist.py
@@ -1,18 +1,20 @@
 '''
-- generate random number
-- 
+Generate uniform random numbers:
+250 random floats from 0.0 to 5.0
+  then show the histogram divided by 5 datas
+100000 random floats from 0.0 to 5.0
+  then show the histogram divided by 100 datas
 '''
 #!/bin/python
 
 import matplotlib.pyplot as plt
 import numpy
 
-x = numpy.random.uniform(0.0, 5.0, 250)
-y = numpy.random.uniform(0.0, 5.0, 100000)
+r_1 = numpy.random.uniform(0.0, 5.0, 250)
+r_2 = numpy.random.uniform(0.0, 5.0, 100000)
 
-# print(x)
+# print(r_1)
 
-# plt.hist(x, 5)
-# plt.show()
-plt.hist(y, 100)
+# plt.hist(r_1, 5)
+plt.hist(r_2, 100)
 plt.show()
